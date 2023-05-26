cf3a5d655d5be1a9236e6aad3c6d56a295121103
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-05-26 15:37:51 -0500

    restructuring project

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+})
