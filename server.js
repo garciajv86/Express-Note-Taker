37164f0694238abc30c0ba99dbc6b147b8979b58
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-05-26 13:49:36 -0500

    starter files and code

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const port = process.env.PORT || 3001
+
+const app = express();
+
+//TODO: Set up middleware that is needed
+//* Middleware to parse incoming request bodies in JSON format to a Javascript object.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
