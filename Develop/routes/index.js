0bec729cd7e005abc91ffb3d48184b20a4352320
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-05-28 12:11:14 -0500

    added a delete method to delete notes

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -3,13 +3,12 @@ const routes = require("./homePage.js");
 const notesRoute = require("./notes.js");
 const fs = require("fs");
 const uniqueId = require("../helpers/uuid.js");
-const path = require('path');
-
+const path = require("path");
 
 router.use("/notes", notesRoute);
 router.use("/", routes);
 
-//* Retrieves the notes
+//* --------------------Retrieves the notes-------------------- *//
 router.get("/api/notes", (req, res) => {
   fs.readFile(path.join(__dirname, "..", "db", "db.json"), (err, data) => {
     if (err) {
@@ -31,12 +30,14 @@ router.get("/api/notes", (req, res) => {
   });
 });
 
-//* Saves new notes
-router.post('/api/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), (err, data) => {
+//* --------------------Saves new notes-------------------- *//
+router.post("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "..", "db", "db.json"), (err, data) => {
     if (err) {
       console.error(err);
-      res.status(500).json({ error: 'An error occurred while reading the file' });
+      res
+        .status(500)
+        .json({ error: "An error occurred while reading the file" });
       return;
     }
     try {
@@ -50,12 +51,14 @@ router.post('/api/notes', (req, res) => {
 
       // Save the updated notes array back to the JSON file
       fs.writeFile(
-        path.join(__dirname, '..', 'db', 'db.json'),
+        path.join(__dirname, "..", "db", "db.json"),
         JSON.stringify(notes),
         (err) => {
           if (err) {
             console.error(err);
-            res.status(500).json({ error: 'An error occurred while saving the note.' });
+            res
+              .status(500)
+              .json({ error: "An error occurred while saving the note." });
             return;
           }
 
@@ -64,10 +67,57 @@ router.post('/api/notes', (req, res) => {
       );
     } catch (err) {
       console.error(err);
-      res.status(500).json({ error: 'An error occurred while parsing the JSON data.' });
+      res
+        .status(500)
+        .json({ error: "An error occurred while parsing the JSON data." });
     }
   });
 });
-//* Deletes note
+
+//* --------------------Deletes note-------------------- *//
+router.delete("/api/notes/:id", (req, res) => {
+  fs.readFile(path.join(__dirname, "..", "db", "db.json"), (err, data) => {
+    if (err) {
+      console.error(err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while reading the file" });
+    }
+    try {
+      const notes = JSON.parse(data);
+      const noteID = req.params.id;
+
+      const index = notes.findIndex((note) => note.id === noteID);
+
+      if (index !== -1) {
+        //* Removes the note at the specified index
+        notes.splice(index, 1);
+        fs.writeFile(
+          path.join(__dirname, "..", "db", "db.json"),
+          JSON.stringify(notes),
+          (err) => {
+            if (err) {
+              console.error(err);
+              res
+                .status(500)
+                .json({ error: "An error occurred while updating the file" });
+            } else {
+              //* Successful deletion, no content to return
+              res.sendStatus(204);
+            }
+          }
+        );
+      } else {
+        //* Note with the specified ID was not found
+        res.status(404).json({ error: "Note not found" });
+      }
+    } catch (err) {
+      console.error(err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while parsing the JSON data." });
+    }
+  });
+});
 
 module.exports = router;
